694e0114551e258a1f876c44d02fb5c2b5b11690
leejongcheal <aksndk123@naver.com>
2020-03-14 21:21:55 +0900

    add phpjs/4.php for compare grammar ex

---
+++ phpjs/4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>js</h1>
+        <script>
+            document.write("2==1 : "+(2==1));
+        </script>
+        <h1>php</h1>
+        <?php 
+            var_dump(1==1);
+        ?>
+    </body>
+</html>
