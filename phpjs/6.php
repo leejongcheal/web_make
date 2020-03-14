6137c3c922ed5916f03d607ef4e8388fe9af1d19
leejongcheal <aksndk123@naver.com>
2020-03-14 21:27:41 +0900

    add phpjs/6.php for js input grammar ex

---
+++ phpjs/6.php
@@ -0,0 +1,17 @@
+<!-- input grammar example -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <script>
+            password = prompt("비밀번호를 입력하세요 : ");
+            if (password == 1111){
+                document.write("비밀번호가 맞습니다.");
+            }else{
+                document.wrtie("비밀번호가 틀립니다.");
+            }
+        </script>
+    </body>
+</html>
