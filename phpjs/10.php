c74308fc26c6cd59adb93230e889801905ee46b0
leejongcheal <aksndk123@naver.com>
2020-03-14 22:34:53 +0900

    add phpjs/10.php for function grammar ex

---
+++ phpjs/10.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>js</h1>
+        <script>
+            function plus_1(a){
+                return a+1;
+            }
+            document.write("function result of 7 : "+ plus_1(7));
+        </script>
+        <h1>php</h1>
+        <?php
+        function plus_1($a){
+            return $a+1;
+        }
+        echo "function result of 6 : ".plus_1(6);
+        ?>
+    </body>
+</html>
