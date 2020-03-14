560b71f2e4e36b39e59487325e21552681e2ec5f
leejongcheal <aksndk123@naver.com>
2020-03-14 20:46:12 +0900

    add phpjs/3.php for variable grammer ex

---
+++ phpjs/3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>js</h1>
+        <script>
+            document.write("10"+"10<br>");
+            name = "lee";
+            document.write("안녕하세요 "+name);
+            // alert(name);
+        </script>
+        <h1>php</h1>
+        <?php
+            echo "10"+"10"."<br>";
+            $name = "lee";
+            echo "안녕하세요 ".$name;
+        ?>
+    </body>
+</html>
