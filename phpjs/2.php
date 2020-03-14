cba40950a34659e1b09ac9848f5e23d6c55c5edb
leejongcheal <aksndk123@naver.com>
2020-03-14 20:45:26 +0900

    add phpjs/2.php for num grammer

---
+++ phpjs/2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>js</h1>
+        <script>
+            document.write("10"+"10");
+        </script>
+        <h1>php</h1>
+        <?php
+            echo "10"+"10";
+        ?>
+    </body>
+</html>
