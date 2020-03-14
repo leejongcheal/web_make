7c7ac2302ed465b9f48d5781582a50a6e8ff767f
leejongcheal <aksndk123@naver.com>
2020-03-14 19:57:52 +0900

    add phpjs/1.php for php and js basic grammer ex

---
+++ phpjs/1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>php</h1>
+        <?php
+            echo "Hello world";
+        ?>
+        <h1>JavaScript</h1>
+        <script>
+            document.write("Hello world");
+        </script>
+    </body>
+</html>
