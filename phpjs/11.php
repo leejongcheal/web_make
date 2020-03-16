c30c93cf52eda3d569a4dd7b658c27d07e7c80ca
leejongcheal <aksndk123@naver.com>
2020-03-16 21:20:17 +0900

    mod phpjs/10.php
    보안처리를 위해 수정하였다.
    mysqli_real_escape_string 함수를 어떤식으로 쓰고 어떤식으로 지켜주는지
    확인 할수있도록 코딩되어 있다,

--- phpjs/11.php
+++ phpjs/11.php
@@ -1,8 +1,10 @@
 <?php 
 	$conn = mysqli_connect('localhost','root','a4850440');// DB에 접속
     mysqli_select_db($conn,'opentutorials'); // 데이터베이스 선택
-    $name = $_GET['name'];
-    $password = $_GET['password'];
+    $name = mysqli_real_escape_string($conn,$_GET['name']);
+    $password = mysqli_real_escape_string($conn,$_GET['password']);
+    // mysql 에선 11'1 인경우 11\'1 로 써야 '을 넣어줄수 있다.
+    // 위의 함수는 11'1이 두번째 인자인경우 11\'1 을 반환함 이를 이용해서 보안을 할수있음
     $sql = "select * from user where name='".$name."' AND password='".$password."'";
     // 만약 공격자가 password= 1111' or '1'='1 이라고 한다면?
     // 모든 정보가 털림 --> 보안 필요
@@ -17,7 +19,7 @@
     </head>
     <body>
         <?php 
-            if($result->num_rows == 1){
+            if($result->num_rows >= 1){
                 echo $password." 는 비밀번호가 맞습니다.";
             }
             else{
