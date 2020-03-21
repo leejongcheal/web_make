835ac97f34578dec5b7403835089123978e14eae
leejongcheal <aksndk123@naver.com>
2020-03-21 15:37:59 +0900

    mysql/index,style 코드 수정
    jumbotron 클래스와 image 클래스로 이미지부분 둥그렇게 + 박스형 시켰고
    container 클래스로 전체크기 비율에 맞춰 움직이도록 수정함

--- mysql/index.php
+++ mysql/index.php
@@ -16,47 +16,50 @@
 		<link href="http://localhost/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
 	</head>
 	<body id="target">
-		<header>
-			<img src="https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png" alt="생활코딩">
-			<h1><a href="http://localhost/mysql/index.php">JavaScript</a></h1>
-		</header>
-		<div class = "row">
-			<nav class ="col-md-3">
-				<ol>
+		<div class = "container-fluid">
+			<header class ="jumbotron text-center">
+				<img id="logo" class = "img-circle" src="https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png" alt="생활코딩">
+				<h1><a href="http://localhost/mysql/index.php">JavaScript</a></h1>
+			</header>
+			<div class = "row">
+				<nav class ="col-md-3">
+					<ol>
+						<?php
+						while($row = mysqli_fetch_assoc($result)){//쿼리결과의 행을 순차적으로 가져옴
+							echo "<li><a href=http://localhost/mysql/index.php?id=".$row['id'].">".htmlspecialchars($row['title'])."</a></li>\n";
+						}
+						?> 
+					</ol>
+				</nav>
+				<div class ="col-md-9">
+					<div id ="control">
 					<?php
-					while($row = mysqli_fetch_assoc($result)){//쿼리결과의 행을 순차적으로 가져옴
-						echo "<li><a href=http://localhost/mysql/index.php?id=".$row['id'].">".htmlspecialchars($row['title'])."</a></li>\n";
-					}
-					?> 
-				</ol>
-			</nav>
-			<div class ="col-md-9">
-				<div id ="control">
-				<?php
-						echo file_get_contents("button_f.txt");
-				?>
-					<a href="http://localhost/mysql/write.php">쓰기</a>
-				</div>
-				<article>
-					<?php 
-						if(!empty($_GET['id'])){
-							$sql = "select topic.id, title, author, user.name, description 
-							from topic left join user on topic.author = user.id where topic.id = ".$_GET['id'];
-							$result = mysqli_query($conn,$sql);
-							$row = mysqli_fetch_assoc($result);
-							echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
-							echo "<p>author : ".htmlspecialchars($row['name'])."</p>";
-							echo strip_tags($row['description'],"<a><h1><h2><h3><h4><h5>
-							<ul><ol><li>");
-						}
-						else{
-							echo "대문 입니다.";
-						}
+							echo file_get_contents("button_f.txt");
 					?>
-				</article>			
+						<a href="http://localhost/mysql/write.php">쓰기</a>
+					</div>
+					<article>
+						<?php 
+							if(!empty($_GET['id'])){
+								$sql = "select topic.id, title, author, user.name, description 
+								from topic left join user on topic.author = user.id where topic.id = ".$_GET['id'];
+								$result = mysqli_query($conn,$sql);
+								$row = mysqli_fetch_assoc($result);
+								echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
+								echo "<p>author : ".htmlspecialchars($row['name'])."</p>";
+								echo strip_tags($row['description'],"<a><h1><h2><h3><h4><h5>
+								<ul><ol><li>");
+							}
+							else{
+								echo "대문 입니다.";
+							}
+						?>
+					</article>			
+				</div>
+				
 			</div>
-			
 		</div>
+		
 		<!-- jQuery (부트스트랩의 자바스크립트 플러그인을 위해 필요합니다) -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     	<!-- 모든 컴파일된 플러그인을 포함합니다 (아래), 원하지 않는다면 필요한 각각의 파일을 포함하세요 -->
