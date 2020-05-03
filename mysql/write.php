224148d30a9dc13222ab3e801e3f4160a730e488
leejongcheal <aksndk123@naver.com>
2020-05-04 00:55:18 +0900

    index write 파일 수정
    사진 삭제및 css파일 관련해서 링크문제 해결

--- mysql/write.php
+++ mysql/write.php
@@ -11,22 +11,22 @@
 		<meta charset = "utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="http://localhost/mysql/style.css?after">
+		<link rel="stylesheet" type="text/css" href="style.css?after">
 		  <!-- 부트스트랩 -->
-		<link href="http://localhost/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href="http://192.168.43.181/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
 	</head>
 	<body id="target">
 		<div class = "container-fluid">
 			<header class ="jumbotron text-center">
-				<img id="logo" class = "img-circle" src="https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png" alt="생활코딩">
-				<h1><a href="http://localhost/mysql/index.php">JavaScript</a></h1>
+				<!-- <img id="logo" class = "img-circle" src="https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png" alt="생활코딩"> -->
+				<h1><a href="http://192.168.43.181/mysql/index.php">JavaScript</a></h1>
 			</header>
 			<div class = "row">
 				<nav class ="col-md-3">
 					<ol class= "nav nav-pills nav-stacked">
 						<?php
 						while($row = mysqli_fetch_assoc($result)){//쿼리결과의 행을 순차적으로 가져옴
-							echo "<li><a href=http://localhost/mysql/index.php?id=".$row['id'].">".htmlspecialchars($row['title'])."</a></li>\n";
+							echo "<li><a href=http://192.168.43.181/mysql/index.php?id=".$row['id'].">".htmlspecialchars($row['title'])."</a></li>\n";
 						}
 						?> 
 					</ol>
@@ -59,7 +59,7 @@
 							<input type="button" value="white" onclick="document.getElementById('target').className='white'" class="btn btn-default  btn-lg"/>
 							<input type="button" value="black" onclick="document.getElementById('target').className='black'" class="btn btn-default btn-lg"/>
 						</div>
-						<a href="http://localhost/write.php" class="btn btn-success  btn-lg">쓰기</a>
+						<a href="http://192.168.43.181/write.php" class="btn btn-success  btn-lg">쓰기</a>
 					</div>
     			</div>
     		</div>		
@@ -67,7 +67,7 @@
 		<!-- jQuery (부트스트랩의 자바스크립트 플러그인을 위해 필요합니다) -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     	<!-- 모든 컴파일된 플러그인을 포함합니다 (아래), 원하지 않는다면 필요한 각각의 파일을 포함하세요 -->
-    	<script src="http://localhost/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+    	<script src="http://192.168.43.181/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
 	</body>
 </html>
 
